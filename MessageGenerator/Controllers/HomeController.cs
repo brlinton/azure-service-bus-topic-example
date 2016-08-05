1a8d8c8cb11e3aa9a9860cf856ee9968eb623813
Brandon Linton <linton.accounts@gmail.com>
2016-08-05 10:01:40 -0400

    Creating publish / subscriber sample

---
+++ MessageGenerator/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Microsoft.ServiceBus;
+using System.Dynamic;
+using System.Configuration;
+using Microsoft.ServiceBus.Messaging;
+
+namespace MessageGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _topic;
+
+        public HomeController()
+        {
+            _topic = ConfigurationManager.AppSettings["TopicName"];
+        }
+
+        [HttpGet]
+        public ActionResult Home()
+        {
+            MakeSureTopicExists();
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Home(string id)
+        {
+            var connectionString = GetConnectionString();
+            var client = TopicClient.CreateFromConnectionString(connectionString, _topic);
+
+            for (var i = 0; i < 10; i++)
+            {
+                // Create message, passing a string message for the body.
+                var message = new BrokeredMessage("Test message " + i);
+
+                // Set additional custom app-specific property.
+                message.Properties["MessageNumber"] = i;
+
+                // Send message to the topic.
+                client.Send(message);
+            }
+
+            return View();
+        }
+
+        private string GetConnectionString()
+        {
+            var json = System.IO.File.ReadAllText(Server.MapPath(@"~/App_Data/secrets.json"));
+            var converter = new ExpandoObjectConverter();
+            dynamic secrets = JsonConvert.DeserializeObject<ExpandoObject>(json, converter);
+            return secrets.ServiceBusConnectionString;
+        }
+
+        private void MakeSureTopicExists()
+        {
+            var connectionString = GetConnectionString();
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.TopicExists(_topic))
+            {
+                namespaceManager.CreateTopic(_topic);
+            }
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Configuration;
+using System.Dynamic;
+using System.Threading;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topic = ConfigurationManager.AppSettings["Topic"];
+            var subscription = ConfigurationManager.AppSettings["Subscription"];
+            var connectionString = GetConnectionString();
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            RegisterSubscription(namespaceManager, topic, subscription);
+
+            var client = SubscriptionClient.CreateFromConnectionString(connectionString, topic, subscription);
+            RegisterOnMessageCallback(client);
+
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    Thread.Sleep(1000);
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        private static void RegisterSubscription(NamespaceManager namespaceManager, string topic, string subscription)
+        {
+            if (!namespaceManager.SubscriptionExists(topic, subscription))
+            {
+                namespaceManager.CreateSubscription(topic, subscription);
+            }
+        }
+
+        private static void RegisterOnMessageCallback(SubscriptionClient client)
+        {
+            var options = new OnMessageOptions();
+            options.AutoComplete = false;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
+
+            client.OnMessage((message) =>
+            {
+                try
+                {
+                    // Process message from subscription.
+                    Console.WriteLine("Body: " + message.GetBody<string>());
+                    Console.WriteLine("MessageID: " + message.MessageId);
+                    Console.WriteLine("Message Number: " + message.Properties["MessageNumber"]);
+
+                    // Remove message from subscription.
+                    message.Complete();
+                }
+                catch (Exception)
+                {
+                    // Indicates a problem, unlock message in subscription.
+                    message.Abandon();
+                }
+            }, options);
+        }
+
+        private static string GetConnectionString()
+        {
+            var json = System.IO.File.ReadAllText(@".\secrets.json");
+            var converter = new ExpandoObjectConverter();
+            dynamic secrets = JsonConvert.DeserializeObject<ExpandoObject>(json, converter);
+            return secrets.ServiceBusConnectionString;
+        }
+    }
+}
